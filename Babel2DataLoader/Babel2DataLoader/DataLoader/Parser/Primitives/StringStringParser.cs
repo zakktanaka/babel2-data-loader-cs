0c6008f3c82d16db579fda23511af74db305165d
SS <zakktanaka@hotmail.com>
2019-02-10 22:55:39 +0900

    string <=> string

---
+++ Babel2DataLoader/Babel2DataLoader/DataLoader/Parser/Primitives/StringStringParser.cs
@@ -0,0 +1,15 @@
+﻿namespace Babel2.DataLoader.Parser.Primitives
+{
+    class StringStringParser : AbstractStringParser<string>
+    {
+        public override string ConvertFrom(string stringvalue)
+        {
+            return stringvalue;
+        }
+
+        public override string ConvertTo(string objectvalue)
+        {
+            return objectvalue;
+        }
+    }
+}
---
+++ Babel2DataLoader/Babel2DataLoaderTests/DataLoader/Parser/Primitives/TestStringStringParser.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Babel2.DataLoader.Parser.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Babel2.DataLoader.Parser.Primitives.Tests
+{
+    [TestClass()]
+    public class TestStringStringParser
+    {
+        [TestMethod()]
+        public void StringStringParser_ConvertObjectTest()
+        {
+            var rep = StringParserRepository.DefaultRepository;
+            var isp = rep.GetStringParser(typeof(string));
+            Assert.AreEqual("HOGE", isp.ConvertObjectTo("HOGE"));
+            Assert.AreEqual("HOHE", (string)isp.ConvertObjectFrom("HOHE"));
+        }
+        [TestMethod()]
+        public void StringStringParser_ConvertTest()
+        {
+            var rep = StringParserRepository.DefaultRepository;
+            var isp = rep.GetStringParser<string>();
+            Assert.AreEqual("HOGE", isp.ConvertTo("HOGE"));
+            Assert.AreEqual("HOHE", isp.ConvertFrom("HOHE"));
+        }
+    }
+}
