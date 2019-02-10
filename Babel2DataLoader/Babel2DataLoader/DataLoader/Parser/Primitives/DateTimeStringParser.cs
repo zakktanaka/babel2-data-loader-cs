52953b5dc95dcac3887e61c1fe0b3cb477715389
SS <zakktanaka@hotmail.com>
2019-02-10 23:20:21 +0900

    datetime <=> string

---
+++ Babel2DataLoader/Babel2DataLoader/DataLoader/Parser/Primitives/DateTimeStringParser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Babel2.DataLoader.Parser.Primitives
+{
+    class DateTimeStringParser : AbstractStringParser<DateTime>
+    {
+        public override DateTime ConvertFrom(string stringvalue)
+        {
+            return DateTime.Parse(stringvalue);
+        }
+
+        public override string ConvertTo(DateTime objectvalue)
+        {
+            return objectvalue.ToString();
+        }
+    }
+}
---
+++ Babel2DataLoader/Babel2DataLoaderTests/DataLoader/Parser/Primitives/TestDateTimeStringParser.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Babel2.DataLoader.Parser.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Babel2.DataLoader.Parser.Primitives.Tests
+{
+    [TestClass()]
+    public class TestDateTimeStringParser
+    {
+        [TestMethod()]
+        public void DateTimeStringParser_ConvertObjectTest()
+        {
+            var rep = StringParserRepository.DefaultRepository;
+            var isp = rep.GetStringParser(typeof(DateTime));
+            var ex = DateTime.Now.Date;
+            Assert.AreEqual(ex.ToString(), isp.ConvertObjectTo(ex));
+            Assert.AreEqual(ex, (DateTime)isp.ConvertObjectFrom(ex.ToString()));
+        }
+        [TestMethod()]
+        public void DateTimeStringParser_ConvertTest()
+        {
+            var rep = StringParserRepository.DefaultRepository;
+            var isp = rep.GetStringParser<DateTime>();
+            var ex = DateTime.Now.Date;
+            Assert.AreEqual(ex.ToString(), isp.ConvertTo(ex));
+            Assert.AreEqual(ex, isp.ConvertFrom(ex.ToString()));
+        }
+    }
+}
