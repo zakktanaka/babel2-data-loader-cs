9dcfa07d37d92c4de383661d7bcbda375e3b1242
SS <zakktanaka@hotmail.com>
2019-02-10 22:45:57 +0900

    bool <=> string

---
+++ Babel2DataLoader/Babel2DataLoader/DataLoader/Parser/Primitives/BoolStringConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Babel2.DataLoader.Parser.Primitives
+{
+    class BoolStringConverter : AbstractStringParser<bool>
+    {
+        public override bool ConvertFrom(string stringvalue)
+        {
+            return bool.Parse(stringvalue);
+        }
+
+        public override string ConvertTo(bool objectvalue)
+        {
+            return objectvalue.ToString();
+        }
+    }
+}
---
+++ Babel2DataLoader/Babel2DataLoaderTests/DataLoader/Parser/Primitives/TestBoolStringConverter.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Babel2.DataLoader.Parser.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Babel2.DataLoader.Parser.Primitives.Tests
+{
+    [TestClass()]
+    public class TestBoolStringConverter
+    {
+        [TestMethod()]
+        public void BoolStringConverter_ConvertObjectTest()
+        {
+            var rep = StringParserRepository.DefaultRepository;
+            var isp = rep.GetStringParser(typeof(bool));
+            Assert.AreEqual(true.ToString(), isp.ConvertObjectTo(true));
+            Assert.AreEqual(false.ToString(), isp.ConvertObjectTo(false));
+
+            Assert.IsTrue((bool)isp.ConvertObjectFrom("true"));
+            Assert.IsTrue((bool)isp.ConvertObjectFrom("TRUE"));
+
+            Assert.IsFalse((bool)isp.ConvertObjectFrom("false"));
+            Assert.IsFalse((bool)isp.ConvertObjectFrom("FALSE"));
+        }
+        [TestMethod()]
+        public void BoolStringConverter_ConvertTest()
+        {
+            var rep = StringParserRepository.DefaultRepository;
+            var isp = rep.GetStringParser<bool>();
+            Assert.AreEqual(true.ToString(), isp.ConvertTo(true));
+            Assert.AreEqual(false.ToString(), isp.ConvertTo(false));
+
+            Assert.IsTrue(isp.ConvertFrom("true"));
+            Assert.IsTrue(isp.ConvertFrom("TRUE"));
+
+            Assert.IsFalse(isp.ConvertFrom("false"));
+            Assert.IsFalse(isp.ConvertFrom("FALSE"));
+        }
+    }
+}
