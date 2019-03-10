3a6a8b490de0c6b44d2404f1347a65351c9a484e
SS <zakktanaka@hotmail.com>
2019-03-10 17:32:06 +0900

    add exception

---
+++ Babel2DataLoader/Babel2DataLoader/DataLoader/Exceptions/DataLoaderException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Babel2.DataLoader.Exceptions
+{
+    public class DataLoaderException : Exception
+    {
+        public DataLoaderException()
+        {
+        }
+
+        public DataLoaderException(string message) :base(message)
+        {
+        }
+
+        public DataLoaderException(string message, params object[] args) :
+            this(string.Format(message, args))
+        {
+        }
+    }
+}
